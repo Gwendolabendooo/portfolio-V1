9022f5b7bfa0d01bec573afdd78b3d5478698a69
Gwendolabendooo <58566369+Gwendolabendooo@users.noreply.github.com>
2020-04-21 00:43:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,82 +3,86 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-var c1 = 0;
+window.onscroll = function() {
+    var c1 = 0;
 
-function scrollBar(){
-    var v = window.pageYOffset;
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
+    scrollBar(), posExp(), colorNav();
 
-    if (document.documentElement.scrollTop > document.querySelector("body > header").offsetHeight -78){
-        document.getElementById("myBar").style.width = scrolled + "%";
-        document.querySelector("div.progress-container").style.width = '100%';
-        document.querySelector("div.progress-container").style.top = '0';
-        document.querySelector("div.formesPropos").style.animation = 'scaleUp 1s';
-        document.querySelector("div.formesPropos").style.transform = 'scale(1)';
-        
-    }else{
-        document.querySelector("div.progress-container").style.top = -6 + "px";
-    }
-}
+    function scrollBar(){
+        var v = window.pageYOffset;
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
 
-function posExp(){
-    let htMin = document.querySelector("div.parcours").offsetTop;
-    let htExp = document.querySelector("div.experience").offsetTop;
-    let widthExp = document.querySelector("#experience").offsetHeight / 2
-    let htActuelle = document.documentElement.scrollTop;
-
-    var opacité = 10 * (1 - htActuelle / htExp);
-    if (htActuelle > htExp - widthExp) {
-        document.querySelector("div.ctnParcours").style.opacity = opacité;
-    }else{
-        document.querySelector("div.ctnParcours").style.opacity = 1;
-    }
-    
-    if (htActuelle >= htMin && htExp > htActuelle){
-        document.querySelector("div.ctnParcours").style.position = "fixed";
-        document.querySelector("div.ctnParcours").style.top = 0 + "px";
-        defillement();
-    }else if (htMin > htActuelle) {
-        document.querySelector("div.ctnParcours").style.position = "relative";
-    }else if (htExp <= htActuelle) {
-        document.querySelector("div.ctnParcours").style.position = "relative";
-        document.querySelector("div.ctnParcours").style.top = document.querySelector("div.experience").offsetTop - document.querySelector("body > div.parcours").offsetTop + "px";
-        document.querySelector("div.ctnParcours").style.opacity = 0;
-    }
-}
-
-function defillement() {
-    let htActuelle = document.documentElement.scrollTop;
-    let htExp = document.querySelector("body > div.parcours").offsetTop;
-
-    document.querySelector("#ctnParcours").style.left = (htExp - htActuelle) + 'px';
-  }
-
-function colorNav() {
-    var propos = document.querySelector("#propos").offsetTop;
-    var experience = document.querySelector("body > div.parcours").offsetTop;
-    var competence = document.querySelector("body > div.competences").offsetTop;
-    var projet = document.querySelector("body > div.projets").offsetTop;
-    var contact = document.querySelector("body > div.contact").offsetTop;
-    var footer = document.querySelector("body > footer").offsetTop;
-
-    var listHeight = [propos, experience, competence, projet, contact, footer];
-
-    for(let i = 1; i < 6; i++){
-        if (document.documentElement.scrollTop > listHeight[i - 1] - 10 && document.documentElement.scrollTop <= listHeight[i] - 10) {
-            document.querySelector("div.Alignrubrique > a:nth-child(" + i + ")").style.color = "#33E073";
-            if (listHeight[i] == listHeight[3] && c1 < 1) {
-                animPadding();
-                animPercent();
-                c1++;
-            }
+        if (document.documentElement.scrollTop > document.querySelector("body > header").offsetHeight -78){
+            document.getElementById("myBar").style.width = scrolled + "%";
+            document.querySelector("div.progress-container").style.width = '100%';
+            document.querySelector("div.progress-container").style.top = '0';
+            document.querySelector("div.formesPropos").style.animation = 'scaleUp 1s';
+            document.querySelector("div.formesPropos").style.transform = 'scale(1)';
+            
         }else{
-            document.querySelector("div.Alignrubrique > a:nth-child(" + i + ")").style.color = "white";
+            document.querySelector("div.progress-container").style.top = -6 + "px";
         }
     }
-}
+
+    function posExp(){
+        let htMin = document.querySelector("div.parcours").offsetTop;
+        let htExp = document.querySelector("div.experience").offsetTop;
+        let widthExp = document.querySelector("#experience").offsetHeight / 2
+        let htActuelle = document.documentElement.scrollTop;
+
+        var opacité = 10 * (1 - htActuelle / htExp);
+        if (htActuelle > htExp - widthExp) {
+            document.querySelector("div.ctnParcours").style.opacity = opacité;
+        }else{
+            document.querySelector("div.ctnParcours").style.opacity = 1;
+        }
+        
+        if (htActuelle >= htMin && htExp > htActuelle){
+            document.querySelector("div.ctnParcours").style.position = "fixed";
+            document.querySelector("div.ctnParcours").style.top = 0 + "px";
+            defillement();
+        }else if (htMin > htActuelle) {
+            document.querySelector("div.ctnParcours").style.position = "relative";
+        }else if (htExp <= htActuelle) {
+            document.querySelector("div.ctnParcours").style.position = "relative";
+            document.querySelector("div.ctnParcours").style.top = document.querySelector("div.experience").offsetTop - document.querySelector("body > div.parcours").offsetTop + "px";
+            document.querySelector("div.ctnParcours").style.opacity = 0;
+        }
+    }
+
+    function defillement() {
+        let htActuelle = document.documentElement.scrollTop;
+        let htExp = document.querySelector("body > div.parcours").offsetTop;
+
+        document.querySelector("#ctnParcours").style.left = (htExp - htActuelle) + 'px';
+    }
+
+    function colorNav() {
+        var propos = document.querySelector("#propos").offsetTop;
+        var experience = document.querySelector("body > div.parcours").offsetTop;
+        var competence = document.querySelector("body > div.competences").offsetTop;
+        var projet = document.querySelector("body > div.projets").offsetTop;
+        var contact = document.querySelector("body > div.contact").offsetTop;
+        var footer = document.querySelector("body > footer").offsetTop;
+
+        var listHeight = [propos, experience, competence, projet, contact, footer];
+
+        for(let i = 1; i < 6; i++){
+            if (document.documentElement.scrollTop > listHeight[i - 1] - 10 && document.documentElement.scrollTop <= listHeight[i] - 10) {
+                document.querySelector("div.Alignrubrique > a:nth-child(" + i + ")").style.color = "#33E073";
+                if (listHeight[i] == listHeight[3] && c1 < 1) {
+                    animPadding();
+                    animPercent();
+                    c1++;
+                }
+            }else{
+                document.querySelector("div.Alignrubrique > a:nth-child(" + i + ")").style.color = "white";
+            }
+        }
+    }
+};
 
 function animPadding() {
     var html = document.querySelector("#competence > div:nth-child(2) > div");
@@ -146,7 +150,5 @@ function countdown(elem, size) {
     }
     progress();
 }
-
-window.onscroll = function() {scrollBar(), posExp(), colorNav()};
 
 ReactDOM.render(<App />, document.getElementById('root'));
