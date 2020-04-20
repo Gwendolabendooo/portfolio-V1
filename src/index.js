702ab1074f4ca626307abdc6ddd22634149c4c5c
Gwendolabendooo <58566369+Gwendolabendooo@users.noreply.github.com>
2020-04-21 00:34:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-window.onscroll = function() {scrollBar(), posExp(), colorNav()};
-
 var c1 = 0;
 
 function scrollBar(){
@@ -149,31 +147,6 @@ function countdown(elem, size) {
     progress();
 }
 
-// var precedent = 0;
-// var tier = window.innerHeight / 3;
-
-// function animation() {
-//     if (window.location == "http://localhost:3000/"){
-//         if ( document.documentElement.scrollTop > document.querySelector("div.back-exp").offsetTop - tier ){
-//             document.querySelector("div.barre").classList.add("barrewidth");
-//             document.querySelector("div.explications").classList.add("app-left");
-//             document.querySelector("div.title2").classList.add("app-right");
-//         }
-//     }
-// }
-
-// function hiddmenu() {
-//     var x = document.scrollTop || document.documentElement.scrollTop;
-//     if (( x >  window.innerHeight - 1)  && ( x > precedent ))
-//     {
-//         document.querySelector("nav").classList.add("hidd");
-//         precedent = x - 1;
-//     }
-//     else{
-//         document.querySelector("nav").classList.remove("hidd");
-//         precedent = x + 1;
-//     }
-//     console.log(x);
-// }
+window.onscroll = function() {scrollBar(), posExp(), colorNav()};
 
 ReactDOM.render(<App />, document.getElementById('root'));
