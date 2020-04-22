0e7ca13244073aa56f3ce271112d5ed1b360ca00
LE FLOCH GWENDAL <58566369+Gwendolabendooo@users.noreply.github.com>
2020-04-22 23:41:39 +0200

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -28,39 +28,41 @@ class header extends React.Component {
 
         return (
             <div className="contact" id="contact">
-                <div className="contactBox sendMsg">
-                    <div className="titleCompetences">
-                        Me contacter
+                <div className="ctnContact">
+                    <div className="contactBox sendMsg">
+                        <div className="titleCompetences">
+                            Me contacter
+                        </div>
+                        <form className="boxForm" action="">
+                            <input className="nom np" id="nom" type="text" placeholder="Nom" name="nom" />
+                            <input className="prenom np" id="prenom" type="text" placeholder="Prenom" name="prenom" />
+                            <input className="mail" id="Mail" type="text" placeholder="Mail" name="Mail" />
+                            <textarea  name="message" placeholder="Votre message..."/>
+                            <input className="send" type="submit" value="Envoyer" />
+                            <FontAwesomeIcon className="img-user icoForm" icon={['fas', 'user']} />
+                            <FontAwesomeIcon className="img-user2 icoForm" icon={['fas', 'user']} />
+                            <FontAwesomeIcon className="icoForm img-env" icon={['fas', 'envelope']} />
+                        </form>
                     </div>
-                    <form className="boxForm" action="">
-                        <input className="nom np" id="nom" type="text" placeholder="Nom" name="nom" />
-                        <input className="prenom np" id="prenom" type="text" placeholder="Prenom" name="prenom" />
-                        <input className="mail" id="Mail" type="text" placeholder="Mail" name="Mail" />
-                        <textarea  name="message" placeholder="Votre message..."/>
-                        <input className="send" type="submit" value="Envoyer" />
-                        <FontAwesomeIcon className="img-user icoForm" icon={['fas', 'user']} />
-                        <FontAwesomeIcon className="img-user2 icoForm" icon={['fas', 'user']} />
-                        <FontAwesomeIcon className="icoForm img-env" icon={['fas', 'envelope']} />
-                    </form>
-                </div>
-                <div className="contactBox maps">
-                    <div className="ctnBox">
-                        <div className="info-perso">
-                            <FontAwesomeIcon className="ico-maps" icon={['fas', 'map-marker-alt']} />
-                            Toulouse, France
-                        </div>
-                        <div className="info-perso">
-                            <FontAwesomeIcon className="ico-maps" icon={['fas', 'mobile-alt']} />
-                            06 12 10 05 72
-                        </div>
-                        <div className="info-perso">
-                            <FontAwesomeIcon className="ico-maps" icon={['fas', 'envelope']} />
-                            Gwendal.LeFloch82@gmail.com
-                        </div>
-                        <div className="ctnIframe">
-                            <iframe frameBorder="0" style={{ width: "100%", height: "100%"}} 
-                                src={_url}>
-                            </iframe>
+                    <div className="contactBox maps">
+                        <div className="ctnBox">
+                            <div className="info-perso">
+                                <FontAwesomeIcon className="ico-maps" icon={['fas', 'map-marker-alt']} />
+                                Toulouse, France
+                            </div>
+                            <div className="info-perso">
+                                <FontAwesomeIcon className="ico-maps" icon={['fas', 'mobile-alt']} />
+                                06 12 10 05 72
+                            </div>
+                            <div className="info-perso">
+                                <FontAwesomeIcon className="ico-maps" icon={['fas', 'envelope']} />
+                                Gwendal.LeFloch82@gmail.com
+                            </div>
+                            <div className="ctnIframe">
+                                <iframe frameBorder="0" style={{ width: "100%", height: "100%"}} 
+                                    src={_url}>
+                                </iframe>
+                            </div>
                         </div>
                     </div>
                 </div>
