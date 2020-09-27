edfa4db17f9b73f755cf8d37389d0124b824ddc8
LE FLOCH GWENDAL <58566369+Gwendolabendooo@users.noreply.github.com>
2020-09-27 15:17:16 +0200

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -15,7 +15,7 @@ class header extends React.Component {
           isLoaded: false,
           items: [],
           nom: "",
-          prenom: "",
+          subject: "",
           email: "",
           description: ""
         };
@@ -43,7 +43,7 @@ class header extends React.Component {
         //     console.log(value); 
         //  }
         
-
+        document.querySelector("#contact > svg").classList.add("appartiones");
         console.log("params apres:", formData);
 
         fetch('http://localhost/api/api.php', {
@@ -82,41 +82,45 @@ class header extends React.Component {
         let _url = `https://www.google.com/maps/embed/v1/place?key=${MY_API}&q=43.564706, 1.480144`
 
         const nom = this.state.nom;
-        const prenom = this.state.prenom;
+        const subject = this.state.subject;
         const email = this.state.email;
         const description = this.state.description;
 
         return (
             <div className="contact" id="contact">
+                <svg version="1.1" className="check" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+                    <circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>
+                    <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/>
+                </svg>
                 <div className="ctnContact">
                     <div className="contactBox sendMsg">
                         <div className="titleCompetences2">
                             Me contacter
                         </div>
                         <form className="boxForm" onSubmit={this.handleSubmit} action="">
-                            <input className="mail" id="mail" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} type="text" name="Mail" />
-                            <label for="Mail">Mail</label>
-                            <input className="subject" id="subject" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} type="text" name="Subject" />
-                            <label for="Subject">Sujet</label>
-                            <textarea  name="description" value={this.state.description} onChange={(e) => this.setState({description: e.target.value})} placeholder="Votre message..."/>
+                            <div className="ctn-mail">
+                                <input className="mail" id="mail" type="email" autocomplete="off" required value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} type="text" name="Mail" />
+                                <label id="maillab" for="Mail">Mail</label>
+                            </div>
+                            <div className="ctn-mail">
+                                <input className="subject" id="subject" type="text" required autocomplete="off" value={this.state.subject} onChange={(e) => this.setState({subject: e.target.value})} type="text" name="Subject" />
+                                <label id="sublab" for="Subject">Sujet</label>
+                            </div>
+                            <textarea className="description" name="description" value={this.state.description} onChange={(e) => this.setState({description: e.target.value})} placeholder="Votre message..."/>
                             <input className="send" type="submit" value="Envoyer" />
                         </form>
                     </div>
                     <div className="contactBox maps">
                         <div className="ctnBox">
-                            <div className="info-perso">
-                                <FontAwesomeIcon className="ico-maps" icon={['fas', 'map-marker-alt']} />
-                                Toulouse, France
-                            </div>
-                            <div className="info-perso">
-                                <FontAwesomeIcon className="ico-maps" icon={['fas', 'mobile-alt']} />
-                                06 12 10 05 72
-                            </div>
-                            <div className="info-perso">
-                                <FontAwesomeIcon className="ico-maps" icon={['fas', 'envelope']} />
-                                Gwendal.LeFloch82@gmail.com
-                            </div>
                             <div className="ctnIframe">
+                                <div className="info-perso info-tel">
+                                    <FontAwesomeIcon className="ico-maps" icon={['fas', 'mobile-alt']} />
+                                    06 12 10 05 72
+                                </div>
+                                <div className="info-perso info-mail">
+                                    <FontAwesomeIcon className="ico-maps" icon={['fas', 'envelope']} />
+                                    Gwendal.LeFloch82@gmail.com
+                                </div>
                                 <iframe frameBorder="0" style={{ width: "100%", height: "100%"}} 
                                     src={_url}>
                                 </iframe>
